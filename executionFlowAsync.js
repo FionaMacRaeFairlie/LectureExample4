d9a4ab7917554632310693f04b3fc6a763643024
FionaMacRaeFairlie <f.fairlie@gcu.ac.uk>
2024-02-17 17:02:17 +0000

    update example

--- executionFlowAsync.js
+++ executionFlowAsync.js
@@ -1,12 +1,12 @@
-function function2(){
-    console.log('function 2');
+function function2() {
+    console.log('function 2 callback message');
 }
-function function1(callback){
+function function1(callback) {
     console.log('function 1 first instruction');
-    setTimeout(callback,10000)
+    setTimeout(callback, 10000)
     console.log('function 1 last instruction');
 }
-function executionFlow(){
+function executionFlow() {
     console.log('main started');
     function1(function2);
     console.log('last instruction in main');
